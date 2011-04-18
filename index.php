0ef695e78ada760af54880002151604696ba8466
Rashmi Bachani <rashmi.jus.me@gmail.com>
2011-04-18 23:41:34 +0530

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Hey There";
+$username="user_971db7b5";
+$password="pA3mg5iN;(5_10";
+$hostname="10.194.111.8";
 
+$x=mysql_connect($hostname, $username, $password);
+if(!$x)
+	die("Error".mysql_error());
+else
+	echo "nailed it!!!";
 ?>
